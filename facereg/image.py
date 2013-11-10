9cf33f4e2b2b5048d5517d9bc32a148bf6cc7111
Dietrich Epp <depp@zdome.net>
2013-11-10 14:07:18 -0800

    Move image class to its own module

---
+++ facereg/image.py
@@ -0,0 +1,68 @@
+import os
+
+# PIL
+import Image as pil_image
+import PIL.ExifTags
+
+def exif_tag(name):
+    """Look up the EXIF tag which has the given name."""
+    for k, v in PIL.ExifTags.TAGS.items():
+        if v == name:
+            return k
+    raise KeyError('No such EXIF tag.')
+ORIENTATION = exif_tag('Orientation')
+
+TYPES = {
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.png': 'image/png',
+}
+
+def find_images(path):
+    """Iterate over all images below the given path."""
+    extensions = {ext for ext, mimetype in TYPES.items()
+                  if mimetype.startswith('image/')}
+    for dirpath, dirnames, filenames in os.walk(path):
+        dirnames[:] = [dirname for dirname in dirnames
+                       if not dirname.startswith('.')]
+        for filename in filenames:
+            if filename.startswith('.'):
+                continue
+            base, ext = os.path.splitext(filename)
+            if ext.lower() not in extensions:
+                continue
+            fullpath = os.path.join(dirpath, filename)
+            relpath = os.path.relpath(fullpath, path)
+            yield Image(relpath, fullpath)
+
+class Image(object):
+    __slots__ = ['relpath', 'fullpath']
+    def __init__(self, relpath, fullpath):
+        self.relpath = relpath
+        self.fullpath = fullpath
+
+    @property
+    def type(self):
+        """Gets the mime type of the image.
+
+        This will be None if the mime type could not be detected.
+        """
+        base, ext = os.path.splitext(self.relpath)
+        return TYPES.get(ext.lower())
+
+    def get_exif(self):
+        """Get the EXIF information, or None if there is none."""
+        if self.type != 'image/jpeg':
+            return None
+        try:
+            image = pil_image.open(self.fullpath)
+        except IOError:
+            return None
+        return image._getexif()
+
+    def get_orientation(self):
+        """Get the image orientation."""
+        exif = self.get_exif()
+        if exif:
+            return exif.get(ORIENTATION, 1)
+        return 1
--- facereg/run.py
+++ facereg/run.py
@@ -4,71 +4,7 @@ from pyramid.response import Response
 import argparse
 import os
 import sys
-
-# PIL
-import Image as pil_image
-import PIL.ExifTags
-
-def exif_tag(name):
-    """Look up the EXIF tag which has the given name."""
-    for k, v in PIL.ExifTags.TAGS.items():
-        if v == name:
-            return k
-    raise KeyError('No such EXIF tag.')
-ORIENTATION = exif_tag('Orientation')
-
-TYPES = {
-    '.jpg': 'image/jpeg',
-    '.jpeg': 'image/jpeg',
-    '.png': 'image/png',
-}
-
-def find_images(path):
-    """Iterate over all images below the given path."""
-    extensions = {ext for ext, mimetype in TYPES.items()
-                  if mimetype.startswith('image/')}
-    for dirpath, dirnames, filenames in os.walk(path):
-        dirnames[:] = [dirname for dirname in dirnames
-                       if not dirname.startswith('.')]
-        for filename in filenames:
-            if filename.startswith('.'):
-                continue
-            base, ext = os.path.splitext(filename)
-            if ext.lower() not in extensions:
-                continue
-            yield os.path.relpath(os.path.join(dirpath, filename), path)
-
-class Image(object):
-    __slots__ = ['relpath', 'fullpath']
-    def __init__(self, relpath, fullpath):
-        self.relpath = relpath
-        self.fullpath = fullpath
-
-    @property
-    def type(self):
-        """Gets the mime type of the image.
-
-        This will be None if the mime type could not be detected.
-        """
-        base, ext = os.path.splitext(self.relpath)
-        return TYPES.get(ext.lower())
-
-    def get_exif(self):
-        """Get the EXIF information, or None if there is none."""
-        if self.type != 'image/jpeg':
-            return None
-        try:
-            image = pil_image.open(self.fullpath)
-        except IOError:
-            return None
-        return image._getexif()
-
-    def get_orientation(self):
-        """Get the image orientation."""
-        exif = self.get_exif()
-        if exif:
-            return exif.get(ORIENTATION, 1)
-        return 1
+from . import image
 
 def hello_world(request):
     return Response('Hello %(name)s!' % request.matchdict)
@@ -78,9 +14,8 @@ class App(object):
 
     def __init__(self, picture_path):
         self.picture_path = os.path.abspath(picture_path)
-        for relpath in find_images(self.picture_path):
-            image = Image(relpath, os.path.join(self.picture_path, relpath))
-            print image.relpath, image.get_orientation()
+        for img in image.find_images(self.picture_path):
+            print img.relpath, img.get_orientation()
         sys.exit(0)
 
     @classmethod
