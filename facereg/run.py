27e3e3217e03beb21d8b0a9600c2a7f220003373
Dietrich Epp <depp@zdome.net>
2013-11-10 14:02:42 -0800

    Extract EXIF orientation information from images

--- facereg/run.py
+++ facereg/run.py
@@ -5,10 +5,28 @@ import argparse
 import os
 import sys
 
-EXTENSIONS = set('.jpg .jpeg .png'.split())
+# PIL
+import Image as pil_image
+import PIL.ExifTags
 
-def find_images(path, extensions=EXTENSIONS):
+def exif_tag(name):
+    """Look up the EXIF tag which has the given name."""
+    for k, v in PIL.ExifTags.TAGS.items():
+        if v == name:
+            return k
+    raise KeyError('No such EXIF tag.')
+ORIENTATION = exif_tag('Orientation')
+
+TYPES = {
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.png': 'image/png',
+}
+
+def find_images(path):
     """Iterate over all images below the given path."""
+    extensions = {ext for ext, mimetype in TYPES.items()
+                  if mimetype.startswith('image/')}
     for dirpath, dirnames, filenames in os.walk(path):
         dirnames[:] = [dirname for dirname in dirnames
                        if not dirname.startswith('.')]
@@ -20,6 +38,38 @@ def find_images(path, extensions=EXTENSIONS):
                 continue
             yield os.path.relpath(os.path.join(dirpath, filename), path)
 
+class Image(object):
+    __slots__ = ['relpath', 'fullpath']
+    def __init__(self, relpath, fullpath):
+        self.relpath = relpath
+        self.fullpath = fullpath
+
+    @property
+    def type(self):
+        """Gets the mime type of the image.
+
+        This will be None if the mime type could not be detected.
+        """
+        base, ext = os.path.splitext(self.relpath)
+        return TYPES.get(ext.lower())
+
+    def get_exif(self):
+        """Get the EXIF information, or None if there is none."""
+        if self.type != 'image/jpeg':
+            return None
+        try:
+            image = pil_image.open(self.fullpath)
+        except IOError:
+            return None
+        return image._getexif()
+
+    def get_orientation(self):
+        """Get the image orientation."""
+        exif = self.get_exif()
+        if exif:
+            return exif.get(ORIENTATION, 1)
+        return 1
+
 def hello_world(request):
     return Response('Hello %(name)s!' % request.matchdict)
 
@@ -28,8 +78,9 @@ class App(object):
 
     def __init__(self, picture_path):
         self.picture_path = os.path.abspath(picture_path)
-        self.images = list(find_images(self.picture_path))
-        print self.images
+        for relpath in find_images(self.picture_path):
+            image = Image(relpath, os.path.join(self.picture_path, relpath))
+            print image.relpath, image.get_orientation()
         sys.exit(0)
 
     @classmethod
